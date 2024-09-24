c453a5fd5d4114266b572a1735ac45562486c921
Epi-phany <damilarejesse@gmail.com>
2024-09-24 21:06:19 +0100

    added static root

--- myproject/settings.py
+++ myproject/settings.py
@@ -141,6 +141,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
