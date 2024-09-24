4e57b8b079ca54e776fee08b9ec17f91fc3c9198
Epi-phany <damilarejesse@gmail.com>
2024-09-24 08:40:22 +0100

    added host

--- myproject/settings.py
+++ myproject/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1",'https://blog-app-6tdl.onrender.com']
+ALLOWED_HOSTS = ["localhost", "127.0.0.1",'blog-app-6tdl.onrender.com']
 
 
 # Application definition
